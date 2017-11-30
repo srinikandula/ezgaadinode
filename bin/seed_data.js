7cde949a648554873cfa6d682e783cdd7f6e2036
srinikandula <srini.kandula@shodogg.com>
2017-11-30 10:21:57 +0530

    Adding Other expense type

--- bin/seed_data.js
+++ bin/seed_data.js
@@ -5,3 +5,4 @@
 db.expenseMaster.insert({"expenseName" : "Diesel", "isDefault" : true })
 db.expenseMaster.insert({"expenseName" : "Toll", "isDefault" : true })
 db.expenseMaster.insert({"expenseName" : "Maintenance", "isDefault" : true })
+db.expenseMaster.insert({"expenseName" : "Other", "isDefault" : true })
