15e8740a5f6950583caf597ae45fd7285157c003
srinikandula <srini.kandula@shodogg.com>
2017-11-30 10:15:11 +0530

    Adding validations for saving driver

--- server/apis/driversApi.js
+++ server/apis/driversApi.js
@@ -24,22 +24,6 @@ Drivers.prototype.addDriver = function (jwt, driverInfo, callback) {
     if (!driverInfo.mobile || !Utils.isValidPhoneNumber(driverInfo.mobile)) {
         retObj.messages.push('Mobile number should be of ten digits');
     }
-
-    /*
-    if (!driverInfo.truckId || !Utils.isValidObjectId(driverInfo.truckId)) {
-        console.log('----->', driverInfo.truckId);
-        retObj.messages.push('Invalid truckId');
-    }
-
-
-    if (!driverInfo.licenseValidity) {
-        retObj.messages.push('Invalid license validity date, format should be in YYYY-MM-DD');
-    }
-
-    if (isNaN(Number(driverInfo.salary))) {
-        retObj.messages.push('Invalid salary, should be a number');
-    }*/
-
     if (retObj.messages.length) {
         callback(retObj);
     } else {
@@ -52,7 +36,6 @@ Drivers.prototype.addDriver = function (jwt, driverInfo, callback) {
             driverInfo.accountId = jwt.accountId;
         }
         driverInfo.mobile = Number(driverInfo.mobile);
-        var today = new Date();
         driverInfo.driverId = "DR" + parseInt((Math.random() * 100000)); // Need to fix this
         delete driverInfo.joiningDate;
 
