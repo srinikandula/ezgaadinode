0bee866eb415c2fcbf7515bf7ee83937f53a9915
svprasadk <svprasadk@mtwlabs.com>
2017-12-07 13:19:04 +0530

    Easygaadi Test Cases Updated

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,8 @@ function getUserHome() {
 var localConfigPath = getUserHome() + '/easygaadi-config.json';
 var projectConfigPath = __dirname + '/config.json';
 
-if(process.env.NODE_ENV === "test") {
+
+if(process.env.NODE_ENV === 'test') {
     projectConfigPath = __dirname + '/test_config.json';
 }
 
