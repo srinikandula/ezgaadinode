4ed5d5dd0d33e1db43d976a338197422cafe3b74
mtw02 <naresh.d@mwtlabs.com>
2018-01-19 17:28:37 +0530

    pull request

--- client/js/modules/expenseModule.js
+++ client/js/modules/expenseModule.js
@@ -84,10 +84,13 @@ app.factory('ExpenseService', function ($http) {
                 params: params
             }).then(success, error);
         },
-        getPaybleAmountByPartyId:function(partyId,success,error){
+        getPaybleAmountByPartyId:function(params,success,error){
             $http({
-                url: '/v1/expense/getPaybleAmountByParty/' + partyId,
+                url: '/v1/expense/getPaybleAmountByPartyId',
                 method: "GET",
+                params: {
+                    partyId:params
+                }
             }).then(success, error);
         }
     }
