59fcd1d102ddf2298d55e63b260609c07a9a7e07
mtw02 <naresh.d@mwtlabs.com>
2018-01-23 11:11:40 +0530

    added gps maping api

--- server/routes/gpsRoutes.js
+++ server/routes/gpsRoutes.js
@@ -3,6 +3,7 @@ var AuthRouter = express.Router();
 var OpenRouter = express.Router();
 
 var gps = require('../apis/gpsApi');
+var devices = require('../apis/devicesApi');
 
 OpenRouter.get('/AddDevicePositions', function (req, res) {
     gps.AddDevicePositions(req.query, function (result) {
@@ -10,6 +11,23 @@ OpenRouter.get('/AddDevicePositions', function (req, res) {
     });
 });
 
+AuthRouter.get('/getDevices',function (req,res) {
+    devices.getDevices(function (result) {
+        res.send(result);
+    })
+});
+
+AuthRouter.post('/addDevice',function (req,res) {
+    devices.addDevice(req.jwt,req.body,function (result) {
+        res.send(result);
+    })
+});
+
+AuthRouter.get('/gpsTrackingByMapView',function (req,res) {
+   gps.gpsTrackingByMapView(req.jwt,function (result) {
+       res.send(result);
+   }) ;
+});
 
 module.exports = {
     AuthRouter: AuthRouter,
