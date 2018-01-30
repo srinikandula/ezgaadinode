2ab47cabc4ecf5812f5a7c6026eef7859ce4bec8
sravan <sravan.g@mtwlabs.com>
2018-01-30 19:26:25 +0530

    Services Module added

--- client/js/adminApp.js
+++ client/js/adminApp.js
@@ -21,15 +21,82 @@ app.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
         templateUrl: 'views/partials/admin/customers/customers.html',
         data:{activeTab: 'customers'}
     }).state({
+        name: 'customers.customersLead',
+        url: '/customersLead',
+        templateUrl: 'views/partials/admin/customers/customersLead.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
         name: 'createCustomer',
         url: '/createCustomer',
         templateUrl: 'views/partials/admin/customers/createCustomer.html',
         data:{activeTab: 'customers'}
 
     }).state({
+        name: 'customers.truckOwners',
+        url: '/truckOwners',
+        templateUrl: 'views/partials/admin/customers/truckOwners.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
+        name: 'customers.transporters',
+        url: '/transporters',
+        templateUrl: 'views/partials/admin/customers/transporters.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
+        name: 'customers.commissionAgent',
+        url: '/commissionAgent',
+        templateUrl: 'views/partials/admin/customers/commissionAgent.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
+        name: 'customers.factoryOwners',
+        url: '/factoryOwners',
+        templateUrl: 'views/partials/admin/customers/factoryOwners.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
+        name: 'customers.guest',
+        url: '/guest',
+        templateUrl: 'views/partials/admin/customers/guest.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
+        name: 'customers.restOfAll',
+        url: '/restOfAll',
+        templateUrl: 'views/partials/admin/customers/restOfAll.html',
+        data:{activeTab: 'customers'}
+
+    }).state({
         name: 'services',
         url: '/services',
-        templateUrl: 'views/partials/admin/services.html',
+        templateUrl: 'views/partials/admin/services/serviceDashboard.html',
+        data:{activeTab: 'services'}
+    }).state({
+        name: 'services.gpsAccounts',
+        url: '/gpsAccounts',
+        templateUrl: 'views/partials/admin/services/gpsAccounts.html',
+        data:{activeTab: 'services'}
+    }).state({
+        name: 'services.erpAccounts',
+        url: '/erpAccounts',
+        templateUrl: 'views/partials/admin/services/erpAccounts.html',
+        data:{activeTab: 'services'}
+    }).state({
+        name: 'services.gpsDevices',
+        url: '/gpsDevices',
+        templateUrl: 'views/partials/admin/services/gpsDevices.html',
+        data:{activeTab: 'services'}
+    }).state({
+        name: 'services.deviceManagement',
+        url: '/deviceManagement',
+        templateUrl: 'views/partials/admin/services/deviceManagement.html',
+        data:{activeTab: 'services'}
+    }).state({
+        name: 'services.payments',
+        url: '/payments',
+        templateUrl: 'views/partials/admin/services/payments.html',
         data:{activeTab: 'services'}
     }).state({
         name: 'orderProcess',
