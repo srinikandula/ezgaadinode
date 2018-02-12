c0a1bd624611660997b86d3712d8a3f161915353
Sneha <sneha.t@mtwlabs.com>
2018-02-12 18:32:50 +0530

    api changes for get contact info

--- server/routes/globalRoutes.js
+++ server/routes/globalRoutes.js
@@ -3,8 +3,8 @@ var AuthRouter = express.Router();
 var globalApis = require('../apis/globalApi');
 var logger = require('./../winston/logger')(module);
 
-AuthRouter.get('/getContactInfo',function (req,res) {
-    globalApis.getContactInfo(req.jwt.accountId,req.body.globalAccess,req,function (result) {
+AuthRouter.get('/getContactInfo/:accountId',function (req,res) {
+    globalApis.getContactInfo(req.params.accountId,req.body.globalAccess,req,function (result) {
         res.send(result);
     })
 });
