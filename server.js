048ade8cd50432b1369b61243a10c426fb56317e
srinikandula <srini.kandula@shodogg.com>
2018-02-19 17:20:21 +0530

    Fixing bug for express upgrade

--- server.js
+++ server.js
@@ -88,7 +88,8 @@ app.use(function (req, res, next) {
     if (/^\/v1\//.test(req.url)) {
         next();
     } else {
-         if(req.host.indexOf('cpanel') != -1){
+        var hostName = req.host || req.hostname;
+         if(hostName.indexOf('cpanel') != -1){
             res.sendFile(__dirname + '/client/views/adminIndex.html');
         } else{
             res.sendFile(__dirname + '/client/views/index.html');
