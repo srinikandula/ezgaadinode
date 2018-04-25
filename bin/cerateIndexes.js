4049de17257a1d0a068ceec64fcfcfa0c0a6795b
srinikandula <srini.kandula@shodogg.com>
2018-04-25 12:32:32 +0530

    Creating indexes

---
+++ bin/cerateIndexes.js
@@ -0,0 +1,6 @@
+db.trucks.createIndex({"deviceId":1})
+db.trucks.createIndex({"accountId":1})
+db.trucks.createIndex({"userName":1})
+db.trucks.createIndex({"registrationNo":1})
+
+db.operatingRoutes.createIndex({"destinationLocation" : "2dsphere"})
