9ab3ab2bc7358aa4522d14985fc022ade6d93d20
kalyani7 <email@email.com>
2018-04-26 18:47:59 +0530

    modified gps markers

--- client/js/modules/truckTracking.js
+++ client/js/modules/truckTracking.js
@@ -124,14 +124,14 @@ app.controller('TruckTrackingController', ['$scope', '$state','truckTrackingServ
     function renderPolyline() {
         var flightPathCoordinates=[];
         for (var i = 0; i< locations.length; i++) {
-            flightPathCoordinates.push({lat:locations[i].location.coordinates[1],lng: locations[i].location.coordinates[0]})
+            // console.log("addresss....",locations[i].address);
+            flightPathCoordinates.push({lat:locations[i].location.coordinates[1],lng: locations[i].location.coordinates[0]});
             if(i===0){
-                marker[markerIndex] = new google.maps.Marker({
+                marker = new google.maps.Marker({
                     position: new google.maps.LatLng(locations[i].location.coordinates[1], locations[i].location.coordinates[0]),
-                    icon: green_marker_icon,
+                    icon: '/images/start.png',
                     map: map
                 });
-                markerIndex++;
             } else if(i===locations.length-1){
                 flightPath = new google.maps.Polyline({
                     path: flightPathCoordinates,
@@ -140,18 +140,17 @@ app.controller('TruckTrackingController', ['$scope', '$state','truckTrackingServ
                     strokeOpacity: 1.0,
                     strokeWeight: 2
                 });
-                marker[markerIndex] = new google.maps.Marker({
+                marker = new google.maps.Marker({
                     position: new google.maps.LatLng(locations[i].location.coordinates[1], locations[i].location.coordinates[0]),
-                    icon: red_marker_icon,
+                    icon: '/images/stop.png',
                     map: map
                 });
-                markerIndex++;
                 map.setCenter(flightPathCoordinates[0]);
                 flightPath.setMap(map);
             } else{
                 var icon ={
                     url: '/images/h0.png', // url
-                    scaledSize: new google.maps.Size(15, 15),
+                    scaledSize: new google.maps.Size(15, 15)
                 };
                 var course = parseFloat(locations[i].course);
                 if(course >=25 && course<70){
@@ -177,28 +176,25 @@ app.controller('TruckTrackingController', ['$scope', '$state','truckTrackingServ
                 } else if(course>=450 && course<500){
                     icon.url= '/images/h3.png'
                 }
-                marker[markerIndex] = new google.maps.Marker({
+                marker = new google.maps.Marker({
                     position: new google.maps.LatLng(locations[i].location.coordinates[1], locations[i].location.coordinates[0]),
                     icon: icon,
                     map: map
                 });
-                markerIndex++;
-                /*
                 var d = new Date(locations[i].fixTime);
                 var time = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
                 var infowindow = new google.maps.InfoWindow();
                 var functionContent = '<div>'+'<span> <b>Address:</b></span>'+locations[i].address+'<span><br></span>'+'<span><b>Speed:</b></span>'+locations[i].speed+'<span><br></span>'+'<span> <b>Time:</b></span>'+time+'</div>';
                 var compiledContent = $compile(functionContent)($scope);
-                google.maps.event.addListener(marker[markerIndex], 'click', (function (marker, i, content) {
+                google.maps.event.addListener(marker,'click', (function (marker, i, content) {
                     return function () {
                         infowindow.setContent(content);
                         infowindow.open(map, marker);
                     }
-                })(marker[markerIndex],i, compiledContent[0], $scope)); */
+                })(marker,i, compiledContent[0], $scope));
             }
         }
     }
-
     var symbolTwo = {
         path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, //'M83.7,40.64h-49V6h49Zm-48-1h47V7h-47ZM35,36.86H29.72A1.86,1.86,0,0,1,27.86,35V10.86A1.86,1.86,0,0,1,29.72,9H35V8H29.72a2.86,2.86,0,0,0-2.86,2.86V35a2.86,2.86,0,0,0,2.86,2.86H35Zm-4.48,3a5.35,5.35,0,0,0,3.33-1.95L33,37.32a4.43,4.43,0,0,1-2.57,1.51H11.07c-13.6-15-1.63-29.28-.08-31,6.55-.05,18.81-.14,19.72-.08a2.48,2.48,0,0,1,1.58,1l.83-.56a3.46,3.46,0,0,0-2.35-1.48c-1.19-.08-19.24.07-20,.08h-.21L10.41,7c-.15.16-15,16.26.07,32.69l.15.16H30.51ZM16.67,36.3,16.48,36c-.08-.13-7.69-12.82.36-25.43l.19-.3,7.91,2V34.26Zm.82-24.9c-6.74,10.91-1.41,21.78-.34,23.75l6.78-1.68V13Zm9.08,25,0-1-7.71.18,0,1Zm0-26.52-7.44-.27,0,1,7.44.27ZM80,11H38.36v1H80ZM80,22.6H38.36v1H80Zm0,11.34H38.36v1H80ZM23.49,6.83a.21.21,0,0,1,.11.1,4.83,4.83,0,0,1,.26-1.4c.28-1,.52-1.94,0-2.39a1.24,1.24,0,0,0-1.3,0c-2.05,1-2.21,4-2.21,4.16l1,0S21.46,4.82,23,4L23.16,4a7.62,7.62,0,0,1-.27,1.3c-.29,1.09-.57,2.13.19,2.47Zm.35,36.67c.53-.45.29-1.35,0-2.39a4.83,4.83,0,0,1-.26-1.4.21.21,0,0,1-.11.1l-.41-.91c-.76.34-.49,1.38-.19,2.47a7.53,7.53,0,0,1,.27,1.3L23,42.59c-1.53-.78-1.67-3.29-1.67-3.31l-1,0c0,.13.16,3.12,2.23,4.17a1.83,1.83,0,0,0,.73.19A.83.83,0,0,0,23.84,43.49Z',
         strokeColor: '#393',
