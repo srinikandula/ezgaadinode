010099323907e8677c2fccda6ca8a374fe15329a
srinikandula <srini.kandula@shodogg.com>
2018-05-14 19:00:24 +0530

    Separating client and producer

--- server/apis/gpsApi.js
+++ server/apis/gpsApi.js
@@ -548,7 +548,15 @@ Gps.prototype.editGpsSettings = function (body,req,callback) {
     if(body.minStopTime){
         body.minStopTime=parseInt(body.minStopTime);
     }
-    GpsSettingsColl.update({accountId:req.jwt.id},{$set:body},{upsert: true},function (err,settings) {
+
+    /*
+    stopTime: {type: Number, default: 15},
+    overSpeedLimit: {type: Number, default: 60},
+    minStopTime:{type:Number,default:10},
+    routeNotificationInterval:{type:Number,default:30}
+     */
+    GpsSettingsColl.update({accountId:req.jwt.id},{$set:{"stopTime":body.stopTime,"overSpeedLimit":body.overSpeedLimit,
+            "minStopTime":body.minStopTime,"routeNotificationInterval":body.routeNotificationInterval }},{upsert: true},function (err,settings) {
         if(err){
             retObj.status=false;
             retObj.messages.push('Error retrieving settings for account,'+JSON.stringify(err.message));
