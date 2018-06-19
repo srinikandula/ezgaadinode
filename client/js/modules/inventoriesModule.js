42983e91152eb97a081d1b90b77c6c3fc4c6dc67
kalyani7 <email@email.com>
2018-06-19 17:30:51 +0530

    InventoriesAdded

---
+++ client/js/modules/inventoriesModule.js
@@ -0,0 +1,111 @@
+app.factory('InventoriesService',['$http', '$cookies', function ($http, $cookies) {
+    return{
+        addInventory: function (params, success, error) {
+            $http({
+                url: '/v1/inventories/addInventory',
+                method: "POST",
+                data: params
+            }).then(success, error)
+        },
+        getInventories:function (success, error) {
+            $http({
+                url: '/v1/inventories/get',
+                method: "GET"
+            }).then(success, error)
+        },
+        remove:function (id,success, error) {
+            $http({
+                url: '/v1/inventories/delete/'+id,
+                method: "DELETE"
+            }).then(success, error)
+        },
+        getInventory:function (id,success, error) {
+            $http({
+                url: '/v1/inventories/get/'+id,
+                method: "GET"
+            }).then(success, error)
+        },
+        updateInventory:function (params,success, error) {
+            $http({
+                url: '/v1/inventories/updateIventory',
+                method: "PUT",
+                data:params
+            }).then(success, error)
+        }
+    }
+}]);
+
+app.controller('AddEditInventoryCtrl',['$scope','InventoriesService','$state','$stateParams','Notification',function($scope,InventoriesService,$state,$stateParams,Notification){
+    $scope.title = 'Add Inventory';
+    if($stateParams.Id){
+        $scope.title = 'Update Inventory';
+        InventoriesService.getInventory($stateParams.Id,function(successCallback){
+            if(successCallback.data.status){
+                $scope.inventory = successCallback.data.data;
+                $scope.inventory.date = new Date($scope.inventory.date);
+            }
+        },function(errorCallback){});
+    }
+
+    $scope.add_editInventory = function(){
+        var params = $scope.inventory;
+        if($stateParams.Id){
+            InventoriesService.updateInventory(params,function(successCallback){
+                if(successCallback.data.status){
+                    $state.go('inventories');
+                    Notification.success({message:"Updated Successfully"});
+                }else{
+                    successCallback.data.messages.forEach(function (message) {
+                        Notification.error({ message: message });
+                    });
+                }
+            },function(errorCallback){
+
+            });
+        }else{
+            InventoriesService.addInventory(params,function(successCallback){
+                if(successCallback.data.status){
+                    $state.go('inventories');
+                    Notification.success({message:"Added Successfully"});
+                }else{
+                    successCallback.data.errors.forEach(function (message) {
+                        Notification.error({ message: message });
+                    });
+                }
+
+            },function(errorCallback){
+
+            });
+        }
+
+    };
+    $scope.cancel = function () {
+        $state.go('inventories');
+    }
+}]);
+
+app.controller('InventoryListCtrl',['$scope','InventoriesService','$state','Notification',function($scope,InventoriesService,$state,Notification){
+
+    InventoriesService.getInventories(function(successCallback){
+        if(successCallback.data.status){
+            $scope.inventories = successCallback.data.data;
+        }
+    },function(errorCallback){});
+
+    $scope.goToEditPage = function (id) {
+        $state.go('addInventory',{Id:id});
+
+    };
+    $scope.delete = function (id) {
+        InventoriesService.remove(id,function(successCallback){
+            if(successCallback.data.status){
+                Notification.success({message:"deleted Successfully"});
+            }else{
+                successCallback.data.messages.forEach(function (message) {
+                    Notification.error({ message: message });
+                });
+            }
+        },function(errorCallback){});
+    };
+
+}]);
