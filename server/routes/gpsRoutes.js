f4fe25c60c6cf7ae1f40e615f182ca4d8839cf21
kalyani7 <email@email.com>
2018-10-01 16:16:39 +0530

    GPSRoutes

--- server/routes/gpsRoutes.js
+++ server/routes/gpsRoutes.js
@@ -108,12 +108,12 @@ AuthRouter.post('/shareTripDetailsByVechicleViaEmail',function (req,res) {
 });
 
 
-/*var identifyNotWorkingDevices = cronjob.schedule('*!/30 * * * *', function() {
+var identifyNotWorkingDevices = cronjob.schedule('*/30 * * * *', function() {
     console.log("identify not working devices.....");
     gps.identifyNotWorkingDevices(function (result) {
     });
 });
-identifyNotWorkingDevices.start();*/
+identifyNotWorkingDevices.start();
 
 /*
 
