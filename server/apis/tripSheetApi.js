d5c9fff75093573091c577c18c37fad635d73d06
kalyani7 <email@email.com>
2018-10-15 09:56:02 +0530

    ApiChanged

--- server/apis/tripSheetApi.js
+++ server/apis/tripSheetApi.js
@@ -1,5 +1,6 @@
 var TripSheetsColl = require('./../models/schemas').tripSheetsColl;
 var TrucksColl = require('./../models/schemas').TrucksColl;
+var AccountsColl = require('./../models/schemas').AccountsColl;
 var async = require('async');
 
 
@@ -7,55 +8,89 @@ var TripSheets = function () {
 
 };
 
-TripSheets.prototype.createTripSheet = function (req, callback) {
+function createTripSheet(account,callback){
     var retObj = {
-        status: false,
-        messages: []
+        status:false,
+        messages:[]
     };
     var today = new Date();
-    today = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    TrucksColl.find({accountId: req.jwt.accountId}, function (err, trucks) {
-        if (err) {
+    today = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    TrucksColl.find({accountId:account._id},function(err,trucks){
+        if(err){
             retObj.status = false;
-            retObj.messages.push("Error in finding trucks" + JSON.stringify(err));
+            retObj.messages.push("Error in finding trucks"+JSON.stringify(err));
             callback(retObj);
-        } else if (trucks.length > 0) {
-            async.each(trucks, function (truck, asyncCallback) {
-                TripSheetsColl.find({accountId: req.jwt.accountId, date: today}, function (err, tripSheet) {
-                    if (err || tripSheet.length > 0) {
+        }else if(trucks.length>0){
+            async.each(trucks,function(truck,asyncCallback){
+                TripSheetsColl.find({accountId:account._id,date : today},function(err,tripSheet){
+                    if(err || tripSheet.length>0){
                         asyncCallback(true);
-                    } else {
+                    }else{
                         var tripSheetObj = {
-                            vehicleId: truck._id,
-                            registrationNo: truck.registrationNo,
-                            accountId: req.jwt.accountId,
-                            date: today
+                            vehicleId : truck._id,
+                            registrationNo : truck.registrationNo,
+                            accountId:account._id,
+                            date : today
                         };
                         var tripSheetDoc = new TripSheetsColl(tripSheetObj);
-                        tripSheetDoc.save(function (err, result) {
-                            if (err) {
+                        tripSheetDoc.save(function(err,result){
+                            if(err){
                                 asyncCallback(true);
-                            } else {
+                            }else{
                                 asyncCallback(false);
                             }
                         });
                     }
                 });
-            }, function (err) {
-                if (err) {
+            },function(err){
+                if(err){
                     retObj.status = false;
                     retObj.messages.push("Error in saving trip sheet");
                     callback(retObj);
-                } else {
+                } else{
                     retObj.status = true;
                     retObj.messages.push("Saved successfully");
                     callback(retObj);
                 }
             });
-        } else {
+        }else{
             retObj.status = false;
             retObj.messages.push("No trucks found");
             callback(retObj);
+        }
+    });
+};
+
+TripSheets.prototype.createTripSheet = function (callback) {
+    var retObj = {
+        status:false,
+        messages:[]
+    };
+    AccountsColl.find({},function(err,accounts){
+        if(err){
+            retObj.status = false;
+            retObj.messages.push("Error in finding data"+JSON.stringify(err));
+            callback(retObj);
+        } else{
+            async.each(accounts,function (account,asyncAccCallback) {
+                createTripSheet(account,function(tripSheetCallback){
+                    if(tripSheetCallback.status){
+                        asyncAccCallback(false);
+                    }else{
+                        asyncAccCallback(true);
+                    }
+                });
+            },function(err){
+                if(err){
+                    retObj.status = false;
+                    retObj.messages.push("Error in saving trip sheet");
+                    callback(retObj);
+                } else{
+                    retObj.status = true;
+                    retObj.messages.push("Saved successfully");
+                    callback(retObj);
+                }
+            });
         }
     });
 };
